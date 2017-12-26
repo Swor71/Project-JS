0f4072c2d52424d175b43e7e41bd8f4b56884b43
Marcin Swornowski <swor71@gmail.com>
2017-12-26 13:52:12 +0100

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+/*
+Some comment above
+the second line of that comment
+*/
+
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+//another comment below
