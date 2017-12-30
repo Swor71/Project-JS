3c34f0a4e681f2d395a7b49138b049c4ea95a789
Marcin Swornowski <swor71@gmail.com>
2017-12-30 15:29:38 +0100

    Add half-christmass-tree.js

---
+++ js/half-christmass-tree.js
@@ -0,0 +1,15 @@
+var n = prompt('How many levels does the Christmas Tree have?');
+
+function drawTree(n) {
+	
+	for (var i = 1; i <= n; i++) {
+		var star = '';
+		
+		for(var j = 1; j <= i; j++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+}
+
+drawTree(n);
